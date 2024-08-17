1cb328f1013a027517f7700a0c596eaf5e1864a4
barathraj10 <barathraj.offl@gmail.com>
2024-08-17 11:13:54 +0530

    First Commit

---
+++ src/index.js
@@ -0,0 +1,35 @@
+
+
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(data => {
+        setUsers(data);
+        setLoading(false);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>Users:</h1>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <ul>
+          {users.map(user => (
+            <li key={user.id}>{user.name}</li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
